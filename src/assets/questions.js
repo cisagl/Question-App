8ab423708e88d77f74e2a8a6248c401778e12edb
CISA <cahitisaoglu10@gmail.com>
2024-02-14 16:48:53 +0300

    imgFix

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -9,14 +9,14 @@ const questions = [
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     answer: "Diş Doktoru",
-    media: "./images/pamuk.jpg",
+    media: "https://bergamaplastik.com/slider/sectors/61bb35ed4fa1c.jpg",
   },
   {
     question:
       "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamada bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     answer: "Gülben Ergen",
-    media: "./images/tarkan.jpg",
+    media: "https://image.milimaj.com/i/milliyet/75/750x0/6175ee2f45d2a0a1041973a6.jpg",
   },
   {
     question: "Pteronofobi nedir?",
@@ -27,47 +27,47 @@ const questions = [
       "Kuş tüyüyle gıdıklanma korkusu",
     ],
     answer: "Kuş tüyüyle gıdıklanma korkusu",
-    media: "./images/fobi.jpg",
+    media: "https://www.medicalpark.com.tr/_uploads/_images/_healthGuide/6PY9Ttbt.jpg",
   },
   {
     question:
       "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     answer: "43",
-    media: "./images/balik.jpg",
+    media: "https://blog.tetra.net/tr-tr/wp-content/uploads/2018/09/japon-baligi.jpg",
   },
   {
     question:
       "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     answer: "Mustafa Sandal",
-    media: "./images/bandira.jpg",
+    media: "https://img.tamindir.com/ti_e_ul/elfcan/h/android-icin-en-iyi-10-karaoke-ve-sarki-sozu-uygulamasi_640x360.png",
   },
   {
     question:
       "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     answer: "Hadise",
-    media: "../assets/images/billboard.jpg",
+    media: "https://www.shutterstock.com/image-photo/outdoor-billboard-on-blue-sky-600nw-1921386038.jpg",
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     answer: "Gladyatör Teri",
-    media: "./images/parfum.jpg",
+    media: "https://www.dilaykozmetik.com/productimages/101687/original/jean-paul-gaultier-scandal-le-parfum-intense-her-edp-50-ml.jpg",
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     answer: "Tavuklar",
-    media: "./images/trex.jpg",
+    media: "https://arkeofili.com/wp-content/uploads/2022/10/trrex1.jpeg",
   },
   {
     question:
       "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     answer: "Çerofobi",
-    media: "./images/fobi.jpg",
+    media: "https://api.kidolog.com/nexus/repository/KidologApi/Blog/a1165db4-92b2-423e-9d17-56b9935698ba_kidolog_com_makaleler_fobi_cesitleri_nelerdir_en_yaygin_fobiler_hangileridir_4762f4d217e9613.webp",
   },
 ];
 
