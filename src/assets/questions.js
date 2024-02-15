3871bee1e7621c3c79ed35a0b8eab37fa56d382d
CISA <cahitisaoglu10@gmail.com>
2024-02-15 16:31:01 +0300

    Image changes

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,22 +1,33 @@
+import cinseddi from "../assets/images/cin-seddi.jpg";
+import pamuk from "../assets/images/pamuk.jpg";
+import fobi from "../assets/images/fobi.jpg";
+import balik from "../assets/images/balik.jpg";
+import billboard from "../assets/images/billboard.jpg";
+import mutluluk from "../assets/images/mutluluk.jpg";
+import parfum from "../assets/images/parfum.jpg";
+import tarkan from "../assets/images/tarkan.jpg";
+import trex from "../assets/images/trex.jpg";
+import bandira from "../assets/images/bandira.jpg";
+
 const questions = [
   {
     question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
     options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
     answer: "Pirinç Unu",
-    media: "https://www.indyturk.com/sites/default/files/styles/1368x911/public/article/main_image/2022/11/23/1058611-228941273.jpg?itok=FZA9i2AF",
+    media: cinseddi,
   },
   {
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     answer: "Diş Doktoru",
-    media: "https://bergamaplastik.com/slider/sectors/61bb35ed4fa1c.jpg",
+    media: pamuk,
   },
   {
     question:
       "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamada bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     answer: "Gülben Ergen",
-    media: "https://image.milimaj.com/i/milliyet/75/750x0/6175ee2f45d2a0a1041973a6.jpg",
+    media: tarkan,
   },
   {
     question: "Pteronofobi nedir?",
@@ -27,47 +38,47 @@ const questions = [
       "Kuş tüyüyle gıdıklanma korkusu",
     ],
     answer: "Kuş tüyüyle gıdıklanma korkusu",
-    media: "https://www.medicalpark.com.tr/_uploads/_images/_healthGuide/6PY9Ttbt.jpg",
+    media: fobi,
   },
   {
     question:
       "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     answer: "43",
-    media: "https://blog.tetra.net/tr-tr/wp-content/uploads/2018/09/japon-baligi.jpg",
+    media: balik,
   },
   {
     question:
       "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     answer: "Mustafa Sandal",
-    media: "https://img.tamindir.com/ti_e_ul/elfcan/h/android-icin-en-iyi-10-karaoke-ve-sarki-sozu-uygulamasi_640x360.png",
+    media: bandira,
   },
   {
     question:
       "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     answer: "Hadise",
-    media: "https://www.shutterstock.com/image-photo/outdoor-billboard-on-blue-sky-600nw-1921386038.jpg",
+    media: billboard,
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     answer: "Gladyatör Teri",
-    media: "https://www.dilaykozmetik.com/productimages/101687/original/jean-paul-gaultier-scandal-le-parfum-intense-her-edp-50-ml.jpg",
+    media: parfum,
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     answer: "Tavuklar",
-    media: "https://arkeofili.com/wp-content/uploads/2022/10/trrex1.jpeg",
+    media: trex,
   },
   {
     question:
       "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     answer: "Çerofobi",
-    media: "https://api.kidolog.com/nexus/repository/KidologApi/Blog/a1165db4-92b2-423e-9d17-56b9935698ba_kidolog_com_makaleler_fobi_cesitleri_nelerdir_en_yaygin_fobiler_hangileridir_4762f4d217e9613.webp",
+    media: mutluluk,
   },
 ];
 
