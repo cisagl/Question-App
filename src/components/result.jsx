0f64a5564a0381b590cb2f1a9a53a4888ced499a
CISA <cahitisaoglu10@gmail.com>
2024-02-15 15:18:50 +0300

    updateResult.jsx

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -14,27 +14,42 @@ const Result = () => {
             <div className="rslt">
                 <h1>Test Results</h1>
 
-                <ul className="resultList">
-                    {answers.map((answer, index) => {
-                        let color;
-                        if (answers[index] === questions[index].answer) {
-                            correct++;
-                            color = "green";
-                        } else if (answers[index] === "") {
-                            empty++;
-                            color = "black";
-                        } else {
-                            incorrect++;
-                            color = "red";
-                        }
-                        return (
-                            <li key={index}>
-                                Question {index + 1}:{" "}
-                                <span style={{ color }} className="qA">{answer}</span>{" "}
-                            </li>
-                        );
-                    })}
-                </ul>
+                <div className="uls">
+                    
+                    <ul className="resultList">
+                    <h3>Your answers:</h3>
+                        {answers.map((answer, index) => {
+                            let color;
+                            if (answers[index] === questions[index].answer) {
+                                correct++;
+                                color = "green";
+                            } else if (answers[index] === "") {
+                                empty++;
+                                color = "black";
+                            } else {
+                                incorrect++;
+                                color = "red";
+                            }
+                            return (
+                                <li key={index}>
+                                    Question {index + 1}:{" "}
+                                    <span style={{ color }} className="qA">{answer}</span>{" "}
+                                </li>
+                            );
+                        })}
+                    </ul>
+
+                    <ul className="rightAnswers">
+                    <h3>Right answers:</h3>
+                        {questions.map((question, index) => {
+                                li: return (
+                                    <li key={index}>
+                                        <span style={{ color: "green" }} className="qA">{question.answer}</span>{" "}
+                                    </li>
+                                );
+                            })}
+                    </ul>
+                </div>
 
                 <div className="score">
                     <h2 className="correct">Correct: {correct}</h2>
